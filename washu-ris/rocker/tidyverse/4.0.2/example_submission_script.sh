67a182a99ec9fc9bb40bc29983fb0c663716cc8c
Matthew Schuelke <matt@themadstatter.com>
2025-07-24 14:47:09 -0500

    minor example fixes

--- washu-ris/rocker/tidyverse/4.0.2/example_submission_script.sh
+++ washu-ris/rocker/tidyverse/4.0.2/example_submission_script.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-LSF_DOCKER_VOLUMES='/home/schuelke:/home/schuelke /storage2/fs1/a.wilcox/Active:/storage2/fs1/a.wilcox/Active'
+LSF_DOCKER_VOLUMES_1="/home/schuelke:/home/schuelke"
+LSF_DOCKER_VOLUMES_2="/storage2/fs1/a.wilcox/Active:/storage2/fs1/a.wilcox/Active"
+LSF_DOCKER_VOLUMES="${LSF_DOCKER_VOLUMES_1} ${LSF_DOCKER_VOLUMES_2}"
+PATH="/home/schuelke:$PATH"
 LSF_DOCKER_PORTS='8787:8787'
-bsub -Is -G compute-ohids-condo -q general-interactive -R 'select[port8787=1]' -a 'docker(themadstatter/washu-ris-rocker-tidyverse:4.0.2)' /bin/bash
+bsub -Is -G compute-ohids -q ohids-interactive -R 'select[port8787=1]' -a 'docker(themadstatter/washu-ris-rocker-tidyverse:4.0.2)' /bin/bash
 
 # then at schuelke@compute1-exec-${N}:~$ rstudio-server start
 # and visit http://compute1-exec-${N}.ris.wustl.edu:${PORT}
