db97272e63223d40665735b0b1416ee30af5bd21
Matthew Schuelke <matt@themadstatter.com>
2025-07-25 12:10:19 -0500

    + cmts to exps

--- washu-ris/rocker/tidyverse/4.0.2/example_submission_script.sh
+++ washu-ris/rocker/tidyverse/4.0.2/example_submission_script.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 
-LSF_DOCKER_VOLUMES_1="/home/schuelke:/home/schuelke"
-LSF_DOCKER_VOLUMES_2="/storage2/fs1/a.wilcox/Active:/storage2/fs1/a.wilcox/Active"
-LSF_DOCKER_VOLUMES="${LSF_DOCKER_VOLUMES_1} ${LSF_DOCKER_VOLUMES_2}"
+# map desired volumes to mount locations
+VOLUMES_MAPS=(
+    "/home/schuelke:/home/schuelke"
+    "/storage2/fs1/a.wilcox/Active:/storage2/fs1/a.wilcox/Active"
+)
+
+# collapse maps to space delimited string
+LSF_DOCKER_VOLUMES="${VOLUME_MAPS[*]}"
+
+# prepend path with user home
 PATH="/home/schuelke:$PATH"
+
+# map target port (before colon) to RStudio port (after colon)
 LSF_DOCKER_PORTS='8787:8787'
+
+# submit job
+## -Is creates pseudo-terminal in shell mode
+## -G desired user group
+## -q desired queue
+## -R specify resource requirements
+## -a specify an execution substitution (esub) application with job command
 bsub -Is -G compute-ohids -q ohids-interactive -R 'select[port8787=1]' -a 'docker(themadstatter/washu-ris-rocker-tidyverse:4.0.2)' /bin/bash
 
-# then at schuelke@compute1-exec-${N}:~$ rstudio-server start
-# and visit http://compute1-exec-${N}.ris.wustl.edu:${PORT}
+# when forwarded to schuelke@compute${N}-exec-${NODE}:~$ run `rstudio-server start`
+
+# finally visit http://compute${N}-exec-${NODE}.ris.wustl.edu:${PORT}
